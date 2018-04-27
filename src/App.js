a486be0a87c4a1d1f54986caa38f2b0d9ef0ac97
Daniel Fitz <dfitz360@gmail.com>
2018-04-27 10:13:46 +1000

    Got really basic react app working

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+
+class App extends React.Component {
+    render() {
+        return (
+            <Router>
+                <div>
+                    <Switch>
+                        <Route exact path="/" component={Home} />
+                    </Switch>
+                </div>
+            </Router>
+        );
+    }
+}
+
+export default App;
