b3091f158fcf92f2170c3dfe476b758a2dd15070
Abhi Jagtap <abhijagtap1997@gmail.com>
2018-05-20 17:45:38 +1000

    Fixed Booking Manager :gun:

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     addr: "ws://localhost:7545",
-    mainAccount: "0x5d372cfA9Beee5Beb31d683b219Df2d7C863ABd8",
-    HouseManagerAddr: "0x9B94967373fc61174Da4DBd1f7B4836cF54b8b19",
-    AccountManagerAddr: "0x42F647792F9d7eBeBE8f7430B1bB8FAc851D8227",
-    BookingManagerAddr: "0xe244d34bd57a596a4bBF1aF1Bc2088Ce0a8f4097"
+    mainAccount: "0xf87dc71fFEC781571aBF9e5f3c6FB05772b3b774",
+    HouseManagerAddr: "0x34E42d900e82c14d99Bdd1F356D87a3dDCC4B8c7",
+    AccountManagerAddr: "0x8811153125A63b4E2DF817C291FbE86fa7bA8558",
+    BookingManagerAddr: "0x17EfcF2666162d83519175EC971fb129DcD92d9d"
 };
