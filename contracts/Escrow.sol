a4d27ad4a49c2fb04b2e1580925d868c56e7d1cc
Jeevan Shikaram <jee1shi@me.com>
2018-05-20 16:07:42 +1000

    Escrow Cancellation working

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -15,6 +15,8 @@ contract DBNBEscrow {
     // State of the escrow
     bool    public _renterCheckedIn = false;
     bool    public _ownerCheckedIn = false;
+    bool    public _escrowCancelled = false;
+    bool    public _escrowDefunct = false;
 
     // Constants
     uint256 public TIME_DENOMINATION = 1 minutes;
@@ -26,6 +28,7 @@ contract DBNBEscrow {
     event OwnerCheckedIn();
 
     event EscrowSettlement(uint256 paidToOwner, uint256 returnedToRenter, uint256 balanceRemaining);
+    event EscrowCancellation(uint256 daysSpent, uint256 numberOfDays, uint256 paidToOwner, uint256 returnedToRenter);
 
     // Ensure only the renter or the owner is able to interact with the contract
     modifier onlyOwner() {
@@ -66,6 +69,7 @@ contract DBNBEscrow {
 
     // This function checks in users to indicate that the 
     function checkIn() external onlyInvolved {
+        require(_startTime - 6 hours < block.timestamp, "It's too early to check in. Try again closer to the time of the booking");
         if (msg.sender == _owner) {
             // Owner sent the tx call
             require(_renterCheckedIn, "The renter has not checked in yet");
@@ -81,6 +85,8 @@ contract DBNBEscrow {
     }
 
     function releaseEscrow() external onlyOwner {
+        require(!_escrowDefunct, "Escrow has already ended, cannot do further operations.");
+        require(!_escrowCancelled, "Escrow has been cancelled due to a disagreement.");
         require(_renterCheckedIn && _ownerCheckedIn, "Cannot release funds to listing owner until both parties have checked in.");
         require(_releaseTime < block.timestamp, "The escrow has not matured yet.");
 
@@ -93,7 +99,47 @@ contract DBNBEscrow {
         _renter.transfer(payableToRenter);
 
         // Escrow is now defunct
+        _escrowDefunct = true;
+
         emit EscrowSettlement(payableToOwner, payableToRenter, address(this).balance);
     }
 
+    // Used if there is a diagreement.
+    function cancelEscrow() external onlyInvolved {
+        
+        require(!_escrowDefunct, "Escrow has already ended, cannot do further operations.");
+        // Can only cancel an Escrow if the period hasn't ended
+        require(block.timestamp < _startTime * (TIME_DENOMINATION * _numberOfDays), "Cannot cancel escrow after period has ended and escrow has matured.");
+
+        // Since this is called, the escrow should be cancelled and funds should be reconsiled
+        _escrowCancelled = true;
+
+        // Check if the escrow is being cancelled 24 hours before it starts
+        // TODO: Can allow this variable to be settable by the contract calling this.
+        if (block.timestamp < _startTime - TIME_DENOMINATION) {
+            // If canceled 24 hours before starting, refund to renter without penalty
+            _renter.transfer(address(this).balance);
+        } else {
+            // If canceled less than 24 hours before starting, pay past_days + 1 to the cancelling party and rest to the other party
+
+            // FIXME: the owner has all the power here: they can not cancel and make the renter cancel.
+            
+            // Difference between current time and start time, plus 1 day of penalty
+            uint256 timeSpent = block.timestamp - _startTime;
+            if (timeSpent < 0) {
+                timeSpent = 0;
+            }
+            uint256 daysSpent = uint(timeSpent) / TIME_DENOMINATION;
+
+            uint256 payableToOwner = _costPerDay * (daysSpent);
+            _owner.transfer(payableToOwner);
+
+            uint256 payableToRenter = address(this).balance;
+            _renter.transfer(payableToRenter);
+            emit EscrowCancellation(daysSpent, _numberOfDays, payableToOwner, payableToRenter);
+        }
+
+        // Escrow is now cancelled and is defuncted
+        _escrowDefunct = true;
+    }
 }
